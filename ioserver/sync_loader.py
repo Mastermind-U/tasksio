ede0fd216daf2a3bc5dd50ec181d9835a1580b13
Mastermind-U <rex49513@gmail.com>
2019-11-11 19:25:57 +0000

    added loaders

---
+++ ioserver/sync_loader.py
@@ -0,0 +1,27 @@
+import requests
+from time import time
+
+
+def get_file(url):
+    r = requests.get(url, allow_redirects=True)
+    return r
+
+
+def write_file(response):
+    filename = response.url.split('/')[-1]
+    with open('files/' + filename, 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    start = time()
+    url = 'https://loremflickr.com/320/240'
+
+    for i in range(10):
+        write_file(get_file(url))
+
+    print(time() - start)
+
+
+if __name__ == "__main__":
+    main()
