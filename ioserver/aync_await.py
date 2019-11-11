83e2f2be06261751edae39dd6d9b996b86cd5f62
Mastermind-U <rex49513@gmail.com>
2019-11-11 18:34:56 +0000

    added async example, updated python to 3.8

---
+++ ioserver/aync_await.py
@@ -0,0 +1,32 @@
+import asyncio
+
+
+async def print_nums():
+    num = 1
+    while True:
+        print(num)
+        num += 1
+        await asyncio.sleep(0.1)
+
+
+async def print_time():
+    count = 0
+    while True:
+        if count % 3 == 0:
+            print(f"{count} seconds passed")
+        count += 1
+        await asyncio.sleep(1)
+
+
+async def main():
+    task1 = asyncio.create_task(print_nums())
+    task2 = asyncio.create_task(print_time())
+
+    await asyncio.gather(task1, task2)
+
+
+if __name__ == "__main__":
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+    asyncio.run(main())
